28e26cfab764a011cfaf2ea2ab7b84718e9751a7
Ab <shaharaameer@gmail.com>
2025-02-16 14:39:57 +0400

    removing Open AI key

--- config/config.php
+++ config/config.php
@@ -5,6 +5,5 @@ return [
     'jwt_secret' => '',
     'jwt_expiration' => 3600,
     'sqlite_path'   => __DIR__ . '/../database.sqlite',
-    'openai_api_key'  => '',
     'google_gemini_api_key'  => '',
 ];
